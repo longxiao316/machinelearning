c10b6bf5a9b7e6cdb04bf42a5f15aae5bedecbb3
longxiao316 <l5251280>
2016-07-14 07:03:21 +0800

    kmeans

---
+++ machinelearning/clustering/kmeans/v1.py
@@ -0,0 +1,45 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+def randcenter(data,k):
+    #for simplicity ,just pick up random points
+    ct=[data[random.randint(0,k-1)] for i in range(k)]
+    return ct
+def randcolor():
+    return (random.random(),random.random(),random.random())
+def calcenter(data):
+    ct=[]
+    for d in data:
+        if len(d)>0:
+            ct.append(np.sum(d,axis=0)/len(d))
+    return  ct
+def distance(v1,v2):
+    return np.sum(np.square(v1-v2))
+
+def cluster(data,k):
+    ct=randcenter(data,k)
+    for i in range(10):
+        if(i>0):
+            nct=calcenter(clst)
+            k=len(nct)
+            ct=nct
+        clst=[[] for i in range(k)]
+        for v in data:
+            idx=0
+            dist=-1
+            for j in range(k):
+                c=ct[j]
+                d=distance(c,v)
+                if(d<dist or dist<0):
+                    dist=d
+                    idx=j
+            clst[idx].append(v)
+    return clst
+dt=[random.random()*100000 for i in range(10000)]
+data=np.array(dt).reshape(5000,2)
+mtxs=cluster(data,15)
+for m in mtxs:
+    mtx=np.array(m)
+    if mtx.size > 0:
+        plt.scatter(mtx[:,0],mtx[:,1],color=randcolor())
+plt.show()

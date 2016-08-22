00a72208419a435d18c5232985f26b3302dc732a
1300273082 <1300273082@qq.com>
2016-08-22 23:57:51 +0800

    decision tree

--- machinelearning/DecisionTree/ID3_v2.py
+++ machinelearning/DecisionTree/ID3_v2.py
@@ -3,12 +3,19 @@ import numpy as np
 class ID3V2(object):
     def __init__(self):
         self.data=[]
+        self.model={}
+        self.labels=[]
         return
     def loadData(self,data):
 
         self.data=data
         return
     def train(self):
+        self.labels=set(self.data[:,-1])
+        featurecount = np.shape(self.data)[1]
+        for i in range(featurecount-1):#the last column is tag
+            pass
+        traindata=self.data
         return
     def predict(self,x):
         return
@@ -19,13 +26,17 @@ class ID3V2(object):
             probs.append(float(labels.tolist().count(l))/len(labels))
         return -np.sum(probs*np.log(probs))
     def cal_informationGain_onattribute(self,i):
+        #we will calculate the information gain :
+        # G(D,A)=H(D)-H(D|A)=-sum(p(Di)*ln(p(Di)) - (-sum_i(sum_j(p(Aj)p(Di|Aj)*ln(p(Di|Aj))))
+        #
         #first calculate H(D)
         h_d=self.cal_entropy_onattribute(-1)
+
 
         return 0
 
 a=ID3V2()
 a.loadData(5)
-a.train()
-d=np.arange(9).reshape(3,3)
-print a.cal_entropy_onattribute(d,-1)
+d=np.arange(12).reshape(3,4)
+print d
+print np.shape(d)[1]
---
+++ machinelearning/DecisionTree/ID3_v3.py
@@ -0,0 +1,5 @@
+class ID3_v3(object):
+    def __init__(self):
+        self.tree={}
+    def buildNode(self,data):
+        return
